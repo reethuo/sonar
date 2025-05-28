81dc297b79492ea260948db75dc4b19e2e8ebbb3
reethuo <152209084+reethuo@users.noreply.github.com>
2025-05-28 16:56:42 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,9 @@
-package com.mycompany.app;
+//package com.mycompany.app;
 
 /**
  * Hello world!
  */
-public class App {
+/*public class App {
 
     private static final String MESSAGE = "Hello World!";
 
@@ -22,4 +22,32 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
+}*/
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8082), 0);
+        server.createContext("/", new HelloHandler());
+        server.start();
+        System.out.println("Server started on port 8082");
+    }
+
+    static class HelloHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello from Java HTTP Server!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 }
+
